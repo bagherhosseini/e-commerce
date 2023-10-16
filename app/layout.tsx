d264836b7c8373e91d1a5cae1771346e7b6c86e8
Bagher hosseini <bagherhosseini03@gmail.com>
2023-10-16 21:47:51 +0200

    chore: more margin

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body 
         className={inter.className}>
-        <header className='mt-10'>
+        <header className='mt-16'>
           <Navbar/>
         </header>
         {children}
