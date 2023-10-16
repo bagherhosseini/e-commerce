71ecc5f73119da291ac3d5a11174d9a7948a0578
Jonathan <jonteljungp@gmail.com>
2023-10-16 12:43:16 +0200

    added get-product action

---
+++ actions/get-product.tsx
@@ -0,0 +1,29 @@
+import axios from "axios"
+
+interface Product {
+  id: number,
+  title: string,
+  price: number,
+  description: string,
+  img: string,
+  createdAt: string,
+  categoryId: number,
+  archived: boolean,
+  featured: boolean,
+  sizeId: number,
+  storeId: string
+};
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/getProductById`;
+
+const getProduct = async (id: number): Promise<Product> => {
+  const response = await axios.post(`${URL}`, 
+    { 
+      productId:id
+    }
+  );
+
+  return response.data.products;
+};
+
+export default getProduct;
