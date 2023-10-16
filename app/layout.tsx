0b4ec62b4a9f4e926b3995ea008e1fe26c104fd5
Bagher hosseini <bagherhosseini03@gmail.com>
2023-10-16 20:23:25 +0200

    chore: change nav and colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body 
         className={inter.className}>
-        <header>
+        <header className='mt-10'>
           <Navbar/>
         </header>
         {children}
--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -3,58 +3,47 @@ import React from 'react'
 import Link from 'next/link'
 
 interface Category {
-    id: number,
-    name: string,
-    description: string,
-  }
+  id: number,
+  name: string,
+  description: string,
+}
 
-  interface CategoryItems {
-    item: Category[]
-  }
+interface CategoryItems {
+  item: Category[]
+}
 
-  
 
-  const NavItem: React.FC<CategoryItems>  =  ({
-    item
+
+const NavItem: React.FC<CategoryItems> = ({
+  item
 }) => {
   return (
-    <div className="bg-black text-white">
-    <div className="flex items-center border-b">
-      <div className="logo">
-        <a href="/">
-          <img src="/assets/logo.png" alt="logo" width="100vh" />
-        </a>
-      </div>
-      <nav className="links mx-6 flex items-center space-x-4 lg:space-x-6 ">
-        {item.map((category) => (
-          <Link
-            key={category.id}
-            href={`/category/${category.id}`} // Add the appropriate link
-            className="hover:text-gray-300"
-          >
-            {category.name}
+    <div className="bg-white fixed top-0 w-full shadow-md z-50">
+      <div className="flex items-center border-b">
+        <div className="logo">
+          <a href="/">
+            <img src="/assets/logo.png" alt="logo" width="100vh" />
+          </a>
+        </div>
+        <nav className="links mx-6 flex items-center space-x-4 lg:space-x-6 ">
+          {item.map((category) => (
+            <Link
+              key={category.id}
+              href={`/category/${category.id}`} // Add the appropriate link
+              className="hover:text-gray-300"
+            >
+              {category.name}
+            </Link>
+          ))}
+          <Link className="hover:text-gray-300" href="/about-us">
+            Om oss
           </Link>
-        ))}
-        <Link className="hover:text-gray-300" href="/deals">
-          Erbjudanden
-        </Link>
-        <Link className="hover:text-gray-300" href="/about-us">
-          Om oss
-        </Link>
-        <Link className="hover:text-gray-300" href="/checkout">
-          Kassa
-        </Link>
-      </nav>
-      <div className="search ml-auto flex items-center">
-        <input
-          type="text"
-          className="bg-gray-200 border border-gray-300 rounded-md"
-          placeholder=" Sök produkter..."
-        />
-        <button type="submit">🔎</button>
+          <Link className="hover:text-gray-300" href="/cart">
+            Kassa
+          </Link>
+        </nav>
       </div>
     </div>
-  </div>
   )
 }
 
