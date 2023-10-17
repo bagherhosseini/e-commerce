e0b9a3fa02d3a52f824baf4009e1373f3248bfe0
Bagher hosseini <bagherhosseini03@gmail.com>
2023-10-17 15:36:50 +0200

    fix: vercel not geting latest data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { ProductFE } from "../../components/productFE";
+import { ProductFE } from "../components/productFE";
 
 type Product = {
   id: number;
