1642ab1940382869d12c2dbff2904186dff6ec20
Bagher hosseini <bagherhosseini03@gmail.com>
2023-10-17 13:08:15 +0200

    fix: vercel error

--- components/productFE.tsx
+++ components/productFE.tsx
@@ -50,7 +50,7 @@ export const ProductFE: React.FC<ProductFormProps> = ({
         >
           <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
             <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
-              {latestBillboard.title}
+              {latestBillboard?.title}
             </div>
           </div>
         </div>
